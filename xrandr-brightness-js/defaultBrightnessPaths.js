6d6924544441b6179a1bf7880a880441e8543e4f
Dannel Albert <cuebix@gmail.com>
2019-10-08 00:13:49 -0400

    ~ Typo

--- xrandr-brightness-js/defaultBrightnessPaths.js
+++ xrandr-brightness-js/defaultBrightnessPaths.js
@@ -3,6 +3,6 @@ const path = require('path')
 const paths = {}
 paths.backlightPath = '/sys/class/backlight/intel_backlight'
 paths.brightnessPath = path.join(paths.backlightPath, 'brightness')
-paths.maxBrightessPath = path.join(paths.backlightPath, 'max_brightness')
+paths.maxBrightnessPath = path.join(paths.backlightPath, 'max_brightness')
 
 module.exports = paths
