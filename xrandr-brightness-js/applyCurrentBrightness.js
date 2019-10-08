3484eda3fe30938a8ecfaebfe2b9690081c66585
Dannel Albert <cuebix@gmail.com>
2019-10-08 00:50:41 -0400

    ~ Open file handles

--- xrandr-brightness-js/applyCurrentBrightness.js
+++ xrandr-brightness-js/applyCurrentBrightness.js
@@ -7,11 +7,18 @@ const applyCurrentBrightness = async function (settings = defaultSettings) {
     fsPromises.open(settings.brightnessPath),
     fsPromises.open(settings.maxBrightnessPath)
   ]
+  const openFileHandles = []
 
   return await Promise
     .all(openFilePromises)
-    .then(([brightness, maxBrightness]) => Promise.all([fsPromises.readFile(brightness, 'utf8'), fsPromises.readFile(maxBrightness, 'utf8')]))
+    .then(function ([brightness, maxBrightness]) {
+      openFileHandles.push(brightness, maxBrightness)
+      return Promise.all([fsPromises.readFile(brightness, 'utf8'), fsPromises.readFile(maxBrightness, 'utf8')])
+    })
     .then(([brightness, maxBrightness]) => setBrightness(brightness, maxBrightness))
+    .finally(function () {
+      openFileHandles.forEach(handle => handle.close())
+    })
 }
 
 module.exports = applyCurrentBrightness
