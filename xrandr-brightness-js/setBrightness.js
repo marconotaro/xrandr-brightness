e11d903cb33d6dc8d8bb81264736962324b4eced
Dannel Albert <cuebix@gmail.com>
2019-10-12 12:59:15 -0400

    * 10% min brightness

--- xrandr-brightness-js/setBrightness.js
+++ xrandr-brightness-js/setBrightness.js
@@ -1,13 +1,15 @@
 const xrandr = require('./xrandr')
 const getDeviceId = require('./getDeviceId')
 
-const setBrightness = function (value, maxValue) {
+const defaultMinValue = 0.1
+
+const setBrightness = function (value, maxValue, minValue = defaultMinValue) {
   const deviceId = getDeviceId()
   if (!deviceId) {
     return false
   }
 
-  return xrandr('--output', deviceId, '--brightness', value / maxValue).code === 0
+  return xrandr('--output', deviceId, '--brightness', Math.max(minValue, value / maxValue)).code === 0
 }
 
 module.exports = setBrightness
