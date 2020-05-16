436edc5c513e3b99ff63fc8d6236380e36c04ebb
Dannel Albert <cuebix@gmail.com>
2020-05-16 18:22:14 -0400

    ~ HDMI display being selected instead

--- xrandr-brightness-js/getDeviceId.js
+++ xrandr-brightness-js/getDeviceId.js
@@ -5,6 +5,7 @@ let deviceId
 const getDeviceId = function () {
   if (typeof deviceId === 'undefined') {
     deviceId = xrandr().stdout.split(/[\n\r]+/)
+      .filter(line => !line.includes('HDMI'))
       .filter(line => line.includes('connected primary'))
       .map(line => line.replace(/\s.*$/, ''))
       [0]
